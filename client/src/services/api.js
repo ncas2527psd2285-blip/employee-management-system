9520a2ef601129cd51d8eeb5e6e5b21da3df654a
Thilak <ncas2527psd2285@gmail.com>
2026-07-01 16:35:13 +0530

    Connect frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,14 +4,18 @@ const api = axios.create({
   baseURL: "https://employee-management-system-dfva.onrender.com/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+// Add JWT token to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;

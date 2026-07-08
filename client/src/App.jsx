ec1c8673a5fad0c2344fa06a79fca3f199454f6c
Thilak <ncas2527psd2285@gmail.com>
2026-07-08 10:20:01 +0530

    Add employee photo upload feature and fix login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,9 +29,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route
+                <Route
           path="/dashboard"
           element={
             <ProtectedRoute allowedRoles={["admin"]}>
